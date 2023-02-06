1416e63ec0b716290c6f3de28036551f09595140
Abbissa <carlospozoserrano13@gmail.com>
2023-02-07 00:08:19 +0100

    Añadida la funcionalidad DoG

--- CreateImageFileFromGraphicsObject.java
+++ CreateImageFileFromGraphicsObject.java
@@ -6,23 +6,37 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 import javax.imageio.ImageIO;
 
 public class CreateImageFileFromGraphicsObject {
 
     private static final int N_DIFFS = 5;
-    private static final int R_DIFF = 5;
+    private static final int R_DIFF = 10;
     private static final int SALTO = 1;
 
-    private static final String FILE = "casa.jpeg";
+    private static final String FILE = "imagen1.jpg";
     private static final String FILE_FOLDER = "sourceImg";
     private static final String FILE_DEST_FOLDER = "generatedImg";
 
-    private static final boolean SHUFFLE = false;
     private static final int R_SHUFFLE = 50;
 
     private static final int BATCH_SIZE = 4;
+
+    private static double variance = 0.6;
+    private static double variance_scalar = 1.6;
+
+    private static int radius = 10;
+
+    // values around the midtone greyvalue of the greyvalue
+    private static double threshold = 0.26 * 255; // controls the level above which luminance values will become white
+
+    // values around 20
+    private static double p = 16; // strength of edge sharpening
+
+    // sensitive in low values, less sensitive with larger values
+    private static double phi = 0.01; // sharpness of black and white transition
 
     public static void main(String[] args) throws IOException {
         String FILENAME = createDirs(FILE_DEST_FOLDER + "\\" + FILE);
@@ -30,35 +44,182 @@ public class CreateImageFileFromGraphicsObject {
 
         BufferedImage res = new BufferedImage(bf.getWidth(), bf.getHeight(), BufferedImage.TYPE_INT_RGB);
 
-        blur1(bf, res);
+        // blur1(bf, res);
         // blur2(bf, res);
-        // contour(bf, res);
+        contour(bf, res);
+        
+        long init = System.nanoTime();
+        BufferedImage a = new BufferedImage(bf.getWidth(), bf.getHeight(), BufferedImage.TYPE_INT_RGB);
+        gaussianBlur(bf, a, variance, radius);
+        System.out.println((System.nanoTime() - init) / 1_000_000_000);
+
+        saveImage(FILENAME, a);
+
+        init = System.nanoTime();
+        BufferedImage b = new BufferedImage(bf.getWidth(), bf.getHeight(), BufferedImage.TYPE_INT_RGB);
+        gaussianBlur(bf, b, variance * variance_scalar, radius);
+        System.out.println((System.nanoTime() - init) / 1_000_000_000);
+
+        saveImage(FILENAME, b);
+        // difference(a, b, res, 100, 0.5, 0.01);
+        // difference(a, b, res, 200, 0.5, 0.004);
+        DoG(a, b, res, 200, 21, 0.04);
         saveImage(FILENAME, res);
 
-        BufferedImage res2 = new BufferedImage(bf.getWidth(), bf.getHeight(), BufferedImage.TYPE_INT_RGB);
+        init = System.nanoTime();
+        DoG(a, b, res, threshold, p, phi);
 
-        difference(bf, res, res2);
+        System.out.println((System.nanoTime() - init) / 1_000_000_000);
 
-        saveImage(FILENAME + "\\diff", res2);
-
-        differenceBW(bf, res, res2);
-
-        saveImage(FILENAME + "\\diff", res2);
+        saveImage(FILENAME, res);
 
     }
 
-    private static void difference(BufferedImage bf, BufferedImage res, BufferedImage res2) {
-        for (int i = 0; i < bf.getHeight(); i++) {
-            for (int j = 0; j < bf.getWidth(); j++) {
+    private static void gaussianBlur(BufferedImage bf, BufferedImage res, double variance, int radius) {
 
-                int blue = Math.abs(bf.getRGB(j, i) & 0xff - res.getRGB(j, i) & 0xff);
-                int green = Math.abs(((bf.getRGB(j, i) & 0xff00) >> 8) - ((res.getRGB(j, i) & 0xff00) >> 8));
-                int red = Math.abs(((bf.getRGB(j, i) & 0xff0000) >> 16) - ((res.getRGB(j, i) & 0xff0000) >> 16));
+        double[][] weights = constructWeights(variance, radius);
 
-                res2.setRGB(j, i, (red << 16 | green << 8 | blue));
+        for (int i = 0; i < bf.getWidth(); i++) {
+            for (int j = 0; j < bf.getHeight(); j++) {
+
+                double[][] reds = new double[radius][radius];
+                double[][] greens = new double[radius][radius];
+                double[][] blues = new double[radius][radius];
+
+                for (int j2 = 0; j2 < weights.length; j2++) {
+                    for (int k = 0; k < weights[j2].length; k++) {
+                        int x = i + j2 - (weights.length / 2);
+                        int y = j + k - (weights.length / 2);
+
+                        double weight = weights[j2][k];
+
+                        if (x < 0) {
+                            x = Math.abs(x);
+                        }
+                        if (x > bf.getWidth() - 1) {
+                            x = 2 * bf.getWidth() - x - 2;
+                        }
+                        if (y < 0) {
+                            y = Math.abs(y);
+                        }
+                        if (y > bf.getHeight() - 1) {
+                            y = 2 * bf.getHeight() - y - 2;
+                        }
+                        Color color = new Color(bf.getRGB(x, y));
+
+                        reds[j2][k] = weight * color.getRed();
+                        greens[j2][k] = weight * color.getGreen();
+                        blues[j2][k] = weight * color.getBlue();
+
+                    }
+                }
+                int red = getWeightedValue(reds);
+                int green = getWeightedValue(greens);
+                int blue = getWeightedValue(blues);
+
+                res.setRGB(i, j,
+                        new Color(red, green, blue).getRGB());
 
             }
         }
+    }
+
+    private static double[][] constructWeights(double variance, int radius) {
+        double weights[][] = new double[radius][radius];
+        double sum = 0;
+        for (int i = 0; i < radius; i++) {
+            for (int j = 0; j < radius; j++) {
+                weights[i][j] = gaussianModel(i - radius / 2, j - radius / 2, variance);
+                sum += weights[i][j];
+            }
+
+        }
+
+        System.out.println(sum);
+        double max = 0;
+        for (int i = 0; i < radius; i++) {
+            for (int j = 0; j < radius; j++) {
+                weights[i][j] /= sum;
+                max = Math.max(max, weights[i][j]);
+            }
+        }
+        return weights;
+    }
+
+    private static int getWeightedValue(double[][] weightedColor) {
+        double sum = 0;
+        for (int i = 0; i < weightedColor.length; i++) {
+            for (int j = 0; j < weightedColor.length; j++) {
+                sum += weightedColor[i][j];
+            }
+        }
+        return (int) sum;
+    }
+
+    private static double gaussianModel(int x, int y, double variance) {
+
+        return (1 / (2 * Math.PI * Math.pow(variance, 2))
+                * Math.exp(-(Math.pow(x, 2) + Math.pow(y, 2)) / (2 * Math.pow(variance, 2))));
+    }
+
+    private static void DoG(BufferedImage bf, BufferedImage res, BufferedImage res2, double threshold,
+            double scalar, double phi) {
+        for (int i = 0; i < bf.getHeight(); i++) {
+            for (int j = 0; j < bf.getWidth(); j++) {
+
+                int blue = (int) Math.abs((1 + scalar) * (bf.getRGB(j, i) & 0xff) - scalar * (res.getRGB(j, i) & 0xff));
+                int green = (int) Math
+                        .abs((1 + scalar) * ((bf.getRGB(j, i) & 0xff00) >> 8)
+                                - scalar * ((res.getRGB(j, i) & 0xff00) >> 8));
+                int red = (int) Math
+                        .abs((1 + scalar) * ((bf.getRGB(j, i) & 0xff0000) >> 16)
+                                - scalar * ((res.getRGB(j, i) & 0xff0000) >> 16));
+
+                double col = red + blue + green;
+
+                col /= 3;
+                int color = 255;
+                if (col < threshold) {
+                    color = (int) (127.5 * (1 + Math.tanh(phi * (col - threshold))));
+
+                }
+                res2.setRGB(j, i, new Color(color, color, color).getRGB());
+
+            }
+        }
+    }
+
+    private static void colorDog(BufferedImage bf, BufferedImage res, BufferedImage res2, double threshold,
+            double scalar, double phi) {
+        for (int i = 0; i < bf.getHeight(); i++) {
+            for (int j = 0; j < bf.getWidth(); j++) {
+
+                int blue = (int) Math.abs((1 + scalar) * (bf.getRGB(j, i) & 0xff) - scalar * (res.getRGB(j, i) & 0xff));
+                int green = (int) Math
+                        .abs((1 + scalar) * ((bf.getRGB(j, i) & 0xff00) >> 8)
+                                - scalar * ((res.getRGB(j, i) & 0xff00) >> 8));
+                int red = (int) Math
+                        .abs((1 + scalar) * ((bf.getRGB(j, i) & 0xff0000) >> 16)
+                                - scalar * ((res.getRGB(j, i) & 0xff0000) >> 16));
+
+                if (red < threshold)
+                    red = (int) (127.5 * (1 + Math.tanh(phi * (red - threshold))));
+                else
+                    red = 255;
+                if (green < threshold)
+                    red = (int) (127.5 * (1 + Math.tanh(phi * (green - threshold))));
+                else
+                    green = 255;
+                if (blue < threshold)
+                    blue = (int) (127.5 * (1 + Math.tanh(phi * (blue - threshold))));
+                else
+                    blue = 255;
+
+                res2.setRGB(j, i, new Color(red, green, blue).getRGB());
+
+            }
+        }
+
     }
 
     private static void differenceBW(BufferedImage bf, BufferedImage res, BufferedImage res2) {
@@ -76,20 +237,7 @@ public class CreateImageFileFromGraphicsObject {
         }
     }
 
-    private static String createDirs(String string) {
-        String FILENAME = FILE.split("\\.")[0];
-        File dir = new File(FILE_DEST_FOLDER + "\\" + FILENAME);
-        if (!dir.isDirectory()) {
-            dir.mkdirs();
-        }
-        dir = new File(FILE_DEST_FOLDER + "\\" + FILENAME + "\\diff");
-        if (!dir.isDirectory()) {
-            dir.mkdirs();
-        }
-        return FILENAME;
-    }
-
-    private static void blur2(BufferedImage bf, BufferedImage res) {
+    private static void pixelify(BufferedImage bf, BufferedImage res) {
 
         for (int i = 0; i < bf.getHeight(); i += 2 * BATCH_SIZE) {
             for (int j = 0; j < bf.getWidth(); j += 2 * BATCH_SIZE) {
@@ -139,7 +287,7 @@ public class CreateImageFileFromGraphicsObject {
 
     }
 
-    private static void blur1(BufferedImage bf, BufferedImage res) {
+    private static void pixelMean(BufferedImage bf, BufferedImage res) {
         for (int o = 0; o < N_DIFFS; o++) {
 
             for (int i = 0; i < bf.getHeight(); i++) {
@@ -170,21 +318,33 @@ public class CreateImageFileFromGraphicsObject {
         }
     }
 
-    private static void shuffle(BufferedImage bf, BufferedImage res) {
-        if (SHUFFLE) {
-            Random generator = new Random();
-            int n = R_SHUFFLE;
+    private static void pixelShuffle(BufferedImage bf, BufferedImage res) {
 
-            for (int i = 0; i < bf.getHeight(); i++) {
-                for (int j = 0; j < bf.getWidth(); j++) {
-                    int x = j + generator.nextInt(R_SHUFFLE) - R_SHUFFLE / 2;
-                    int y = i + generator.nextInt(R_SHUFFLE) - R_SHUFFLE / 2;
-                    if (x < 0 || x > bf.getWidth() - 1 || y < 0 || y > bf.getHeight() - 1)
-                        continue;
-                    res.setRGB(j, i, bf.getRGB(x, y));
-                }
+        Random generator = new Random();
+
+        for (int i = 0; i < bf.getHeight(); i++) {
+            for (int j = 0; j < bf.getWidth(); j++) {
+                int x = j + generator.nextInt(R_SHUFFLE) - R_SHUFFLE / 2;
+                int y = i + generator.nextInt(R_SHUFFLE) - R_SHUFFLE / 2;
+                if (x < 0 || x > bf.getWidth() - 1 || y < 0 || y > bf.getHeight() - 1)
+                    continue;
+                res.setRGB(j, i, bf.getRGB(x, y));
             }
         }
+
+    }
+
+    private static String createDirs(String string) {
+        String FILENAME = FILE.split("\\.")[0];
+        File dir = new File(FILE_DEST_FOLDER + "\\" + FILENAME);
+        if (!dir.isDirectory()) {
+            dir.mkdirs();
+        }
+        dir = new File(FILE_DEST_FOLDER + "\\" + FILENAME + "\\diff");
+        if (!dir.isDirectory()) {
+            dir.mkdirs();
+        }
+        return FILENAME;
     }
 
     private static void saveImage(String FILENAME, BufferedImage res) throws IOException {
@@ -201,6 +361,7 @@ public class CreateImageFileFromGraphicsObject {
         }
     }
 
+    // Doesn't work
     private static void contour(BufferedImage bf, BufferedImage res) {
 
         for (int i = 0; i < bf.getHeight(); i++) {
